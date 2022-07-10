813aaf0f9b4c3eea60fe0b3177d29537c629b6f6
jessica <jessica.fernandes.adv@outlook.com>
2022-07-10 13:37:52 -0300

    first commit

---
+++ src/com/dio/primeiroProgramaJava.java
@@ -0,0 +1,17 @@
+package com.dio;
+
+import com.dio.model.gato;
+
+public class primeiroProgramaJava {
+	public static void main (String[] args) {
+	
+	gato gato = new gato(null, null, null);
+	
+	
+	System.out.println(gato);
+	
+	
+	}
+}
+
+
